e6c025cedf872b9d1abbadeb2dab4e1da627a7ab
Memlith <iamcaiosimonassi@gmail.com>
2026-02-23 09:25:01 -0300

    feat: adiciona rota de status

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+//home
 app.get('/', (req, res) => {
   res.send('Fight your fears and survive.')
+})
+
+//status
+app.get('/status', (req, res) => {
+  res.send('API Online')
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`))
