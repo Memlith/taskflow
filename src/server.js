1be2a382c4e19fa3d14959b9942b7bc8ac6d6b0d
Memlith <iamcaiosimonassi@gmail.com>
2026-03-09 09:43:23 -0300

    refactor: altera rota status para sobre

--- src/server.js
+++ src/server.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
   res.send('Sistema de Gerenciamento de Cartas de Magic: The Gathering')
 })
 
-//status
-app.get('/status', (req, res) => {
-  res.send('API Online')
+// sobre
+app.get('/sobre', (req, res) => {
+  res.send('Sistema desenvolvido em Node.js para estudo de CI/CD')
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`))
