31cff06131be1162fe9a2dcc632b1153c2f6baf5
Memlith <iamcaiosimonassi@gmail.com>
2026-06-01 15:23:20 -0300

    fix: ajusta rota para correta

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,8 +2,8 @@ const request = require('supertest')
 const app = require('../src/server')
 
 describe('Testando a rota principal', () => {
-  test('Deve retornar status 200 na rota GET /home', async () => {
-    const response = await request(app).get('/home')
+  test('Deve retornar status 200 na rota GET /', async () => {
+    const response = await request(app).get('/')
     expect(response.statusCode).toBe(200)
   })
 })
