deaccc3ece4da68f048f4529c9acc06c8f78e2e9
Memlith <iamcaiosimonassi@gmail.com>
2026-06-01 14:01:42 -0300

    fix: corrige regra

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,7 +3,7 @@ const app = require('../src/server')
 
 describe('Testando a rota principal', () => {
   test('Deve retornar status 200 na rota GET /', async () => {
-    const response = await request(app).get('/home')
+    const response = await request(app).get('/')
     expect(response.statusCode).toBe(200)
   })
 })
