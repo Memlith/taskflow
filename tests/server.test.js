acb45e40e008023b837d87edc81247eaa3c4cc0d
Memlith <iamcaiosimonassi@gmail.com>
2026-06-01 14:00:38 -0300

    test: simulando erro

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,7 +3,7 @@ const app = require('../src/server')
 
 describe('Testando a rota principal', () => {
   test('Deve retornar status 200 na rota GET /', async () => {
-    const response = await request(app).get('/')
+    const response = await request(app).get('/home')
     expect(response.statusCode).toBe(200)
   })
 })
