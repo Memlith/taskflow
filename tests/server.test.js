84996319571cfed883dbb634b4b97d60bcaad235
Memlith <iamcaiosimonassi@gmail.com>
2026-06-01 14:15:29 -0300

    test: simulando erro para bloqueio

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,8 +2,8 @@ const request = require('supertest')
 const app = require('../src/server')
 
 describe('Testando a rota principal', () => {
-  test('Deve retornar status 200 na rota GET /', async () => {
-    const response = await request(app).get('/')
+  test('Deve retornar status 200 na rota GET /home', async () => {
+    const response = await request(app).get('/home')
     expect(response.statusCode).toBe(200)
   })
 })
